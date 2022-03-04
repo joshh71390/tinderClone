595034f0c03017a13bd6c3e1ca2a4726366a042e
joshh71390 <43319311+joshh71390@users.noreply.github.com>
2022-03-03 23:08:42 -0500

    Update Firebase.js
    bro your gonna get destroyed by indian scammers who will run 50 things on your servers and rack you up thousands of dollars in bills before you even get your first bill...
    
    if your new to coding -> you need to use dotEnv to hide this stuff if you want i can make the changes for you... maybe these are fake to begin with... but it doesnt seem like it its happened to me already you will be crushed... 
    
    https://github.com/dotenv-org/examples/tree/master/dotenv-nodejs
    
    
    your info is probably alredy in about 45 losers databases and they are just waiting to ensure that the servers will stay up.. please do yourself a favor and delete these keys and generate all new ones and never let them come out in the open

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,15 +2,46 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import 'firebase/compat/auth';
 
+
+//step 1 -> put your .env in the git igore 
+
+// step 2 
+require('dotenv').config()
+
+//
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBQ11P3uKVKXyLDABeZBJ3VxJO6Q1XKGRE",
-    authDomain: "tinder-clone-2967f.firebaseapp.com",
-    projectId: "tinder-clone-2967f",
-    storageBucket: "tinder-clone-2967f.appspot.com",
-    messagingSenderId: "670584000437",
-    appId: "1:670584000437:web:9f3f720bf687074a8d53fc",
-    measurementId: "G-0S598ZLXRJ"
-  };
+      apiKey: process.env.FIRE_APIKEY,
+      authDomain: process.env.FIRE_AUTHDOMAIN,
+      databaseURL: process.env.FIRE_DATABASEURL,
+      projectId: process.env.FIRE_PROJECTID,
+      storageBucket: process.env.FIRE_STORAGEBUCKET,
+      messagingSenderId: process.env.FIRE_MESSAGINGSENDERID,
+      appId: process.env.FIRE_APPID
+}
+
+// https://github.com/dotenv-org/examples/tree/master/dotenv-nodejs
+
+// now make a file in the root directory named .env 
+// inside set it up as so 
+
+// KEY=value
+// KEY=value
+
+
+
+// replace your front facing code that you want to hide (*not the truest purpost of .env but 99.95 percent safter then putting your keys below... bots will parse them then just crush you...)
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBQ11P3uKVKXyLDABeZBJ3VxJO6Q1XKGRE",
+//     authDomain: "tinder-clone-2967f.firebaseapp.com",
+//     projectId: "tinder-clone-2967f",
+//     storageBucket: "tinder-clone-2967f.appspot.com",
+//     messagingSenderId: "670584000437",
+//     appId: "1:670584000437:web:9f3f720bf687074a8d53fc",
+//     measurementId: "G-0S598ZLXRJ"
+//   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const database = firebaseApp.firestore();
